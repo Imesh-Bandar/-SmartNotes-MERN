362d6a6d4771649ff70a71f476c4dda7881f3e6f
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-13 01:55:17 +0530

    feat: integrate note routes into server.js for improved API structure

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import noteRoutes from './routes/noteRoutes.js';
 
 const app = express();
 const PORT = 3000;
@@ -8,27 +8,31 @@ const PORT = 3000;
 //http://localhost:3000/api/notes
 
 
-//GET REQUEST API ROUTES
-app.get('/api/notes', (req, res) => {
-    res.status(200).send('GET request to the notes APIs');
-})
+
+app.use("/api/notes", noteRoutes);
 
 
-//CREATE NOTE API ROUTE
-app.post('api/createNote', (req, res) => {
-    res.status(200).send('POST request to create a note');
-})
-
-//UPDATE NOTE API ROUTE
-app.put('/api/updateNote/:id', (req, res) => {
-    res.status(200).send(`PUT request to update note with id ${req.params.id}`);
-})
+// //GET REQUEST API ROUTES
+// app.get('/api/notes', (req, res) => {
+//     res.status(200).send('GET request to the notes APIs');
+// })
 
 
-//DELETE NOTE API ROUTE
-app.delete('/api/deleteNote/:id', (req, res) => {
-    res.status(200).send(`DELETE request to delete note with id ${req.params.id}`);
-});
+// //CREATE NOTE API ROUTE
+// app.post('api/notes/createNote', (req, res) => {
+//     res.status(200).send('POST request to create a note');
+// })
+
+// //UPDATE NOTE API ROUTE
+// app.put('/api/notes/updateNote/:id', (req, res) => {
+//     res.status(200).send(`PUT request to update note with id ${req.params.id}`);
+// })
+
+
+// //DELETE NOTE API ROUTE
+// app.delete('/api/notes/deleteNote/:id', (req, res) => {
+//     res.status(200).send(`DELETE request to delete note with id ${req.params.id}`);
+// });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
