f1778dac5d359908712a03c90d958de73db5af4b
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-13 00:57:21 +0530

    feat: add dev script to package.json and comment out test route in server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const app = express();
 const PORT = 3000;
 
 
+//simple route to test the server
+// app.get('/api/notes', (req, res) => {
+//     res.send('Welcome to the SmartNotes API');
+// });
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
