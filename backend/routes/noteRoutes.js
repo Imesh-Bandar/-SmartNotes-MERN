69d8e50669c7de8c70091c52a3ecccfccd73d401
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-13 01:54:42 +0530

    feat: add note routes for CRUD operations in noteRoutes.js

---
+++ backend/routes/noteRoutes.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import {
+
+    getNotes, createNote, updateNote, deleteNote
+
+} from '../controllers/noteControllers.js'
+
+const router = express.Router();
+
+
+
+
+// @baseURL http://localhost:3000/api/notes
+
+router.get("/", getNotes);
+router.post("/createNote", createNote);
+router.put("/updateNote/:id", updateNote);
+router.delete("/deleteNote/:id", deleteNote);
+
+
+// router.get("/", (req, res) => {
+//     res.status(200).send('GET request to the notes APIs');
+// })
+
+
+
+
+//create note api route
+// router.post("/createNote", (req, res) => {
+//     res.status(201).json({ message: 'POST request to create a note' });
+// })
+
+
+//update note api route
+// router.put("/updateNote/:id", (req, res) => {
+//     res.status(201).json({ message: 'PUT request to UPDATE a note' });
+// });
+
+//delete note api route
+// router.delete("/deleteNote/:id", (req, res) => {
+//     res.status(201).json({ message: 'DELETE request to UPDATE a note' });
+// });
+
+export default router;
