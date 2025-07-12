e502e9b53f8c6a6b63c063cc1b04c204a0b80996
Imesh-Bandar <Imesh.fsd.info@gmail.com>
2025-07-13 02:38:32 +0530

    feat: create Note model with schema for title and content

---
+++ backend/src/models/Notes.mode.js
@@ -0,0 +1,23 @@
+//create schema for notes
+//model based on that schema
+import mongoose from "mongoose";
+
+
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+})
+
+//create model based on the schema
+const Note = mongoose.model("Note", noteSchema);
+//export the model
+export default Note;
